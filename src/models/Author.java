2ca71f0a4cbdf6438f6db71f0cc50eba9432a9be
bogdanlobontiu <bogdan.lobontiu99@e-uvt.ro>
2020-11-24 11:11:34 +0200

    Lab7

---
+++ src/models/Author.java
@@ -0,0 +1,15 @@
+package models;
+
+public class Author {
+
+    private String name;
+
+    public Author(String name){
+        this.name=name;
+    }
+
+    public void print(){
+        System.out.println("models.Author: "+this.name);
+    }
+
+}
---
+++ src/services/AlignCenter.java
@@ -0,0 +1,9 @@
+package services;
+
+public class AlignCenter implements AlignStrategy{
+
+    public void render(String paragraph){
+        System.out.println("+++++"+paragraph+"+++++");
+    }
+
+}
